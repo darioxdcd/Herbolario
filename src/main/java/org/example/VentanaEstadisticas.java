b19fe49010dc34da9eddadc0fdac28e92cb7166a
raulhg16 <raul.hergom.1@educa.jcyl.es>
2025-02-11 18:22:23 +0100

    Update VentanaEstadisticas.java

--- src/main/java/org/example/VentanaEstadisticas.java
+++ src/main/java/org/example/VentanaEstadisticas.java
@@ -3,6 +3,10 @@ package org.example;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -32,21 +36,68 @@ public class VentanaEstadisticas extends JFrame{
 		
 		JPanel estadisticas = new JPanel();
 		add(estadisticas,BorderLayout.CENTER);
+		estadisticas.setLayout(new GridBagLayout());
+		
+		GridBagConstraints constraint = new  GridBagConstraints();
+		constraint.insets = new Insets(30, 250, 400, 300);
+		
+		
+		Font font3 = new Font("Arial", Font.PLAIN, 30);
+		
+		JLabel productos = new JLabel("Nombre: ");
+		productos.setFont(font3);
+		
+		
+		constraint.gridx = 0; 
+		constraint.gridy = 0; 
+		constraint.gridwidth = 1; 
+		constraint.gridheight = 1;
+		
+		estadisticas.add(productos,constraint);
+		
+		JLabel porcentajes = new JLabel("%");
+		porcentajes.setFont(font3);
+		
+		constraint.gridx = 1; 
+		constraint.gridy = 0; 
+		constraint.gridwidth = 1; 
+		constraint.gridheight = 1;
+		
+		
+		estadisticas.add(porcentajes, constraint);
+			
 		
 		//panel botones
 		JPanel botones = new JPanel();
-		botones.setLayout(new FlowLayout());
+		botones.setLayout(new GridBagLayout());
 		add(botones,BorderLayout.SOUTH);
+		
+		constraint.insets = new Insets(30, 50, 10, 50);
+		
 	
 		
 		Font font2 = new Font("Arial", Font.PLAIN, 25);
 		
 		JButton guardar = new JButton("Guardar");
-		botones.add(guardar);
+		
+		constraint.gridx = 0; 
+		constraint.gridy = 0; 
+		constraint.gridwidth = 1; 
+		constraint.gridheight = 1;
+		
+		botones.add(guardar,constraint);
+		
 		guardar.setFont(font2);
 		
 		JButton inicio = new JButton("Inicio");
-		botones.add(inicio);
+		
+		constraint.gridx = 1; 
+		constraint.gridy = 0; 
+		constraint.gridwidth = 1; 
+		constraint.gridheight = 1;
+		
+		botones.add(inicio, constraint);
+		
 		inicio.setFont(font2);
 		
 		
