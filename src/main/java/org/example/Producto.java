88508645ac73d0d1b1fdbdce00f879ee106d949b
Darío García <darioxdcd@gmail.com>
2025-02-11 16:35:32 +0100

    creacion producto

---
+++ src/main/java/org/example/Producto.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+public class Producto
+{
+    float precio;
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+    public Producto(float precio){
+        this.precio = precio;
+    }
+}
