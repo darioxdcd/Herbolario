2cbfa7f51f101a64eadc932a40d58cfa6eaf7338
Darío García <darioxdcd@gmail.com>
2025-02-11 16:43:27 +0100

    pruebas

---
+++ src/main/java/org/example/VentanaInicio.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javax.swing.*;
+
+public class VentanaInicio extends JFrame {
+
+    public VentanaInicio(){
+        setSize(1920,1080);
+        setVisible(true);
+    }
+
+
+}
